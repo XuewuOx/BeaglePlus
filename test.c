86175529aa97e015f54972867953ad50af9c069a
Dan <xuewu.dai@eng.ox.ac.uk>
2013-02-15 12:03:00 +0000

    add two file test.c/test.h

---
+++ test.c
@@ -0,0 +1,16 @@
+/*
+ * test.c
+ *
+ *  Created on: 17 Nov 2012
+ *      Author: Xuewu Daniel Dai 
+ *       Email: xuewu.dai@eng.ox.ac.uk
+ *         URL: http://users.ox.ac.uk/~engs1058/
+ * %              _
+ * %  \/\ /\ /   /  * _  '
+ * % _/\ \/\/ __/__.'(_|_|_
+ */
+
+int testfunc(int x)
+{
+	return 2*x;
+}
---
+++ test.h
@@ -0,0 +1,19 @@
+/*
+ * test.h
+ *
+ *  Created on: 17 Nov 2012
+ *      Author: Xuewu Daniel Dai 
+ *       Email: xuewu.dai@eng.ox.ac.uk
+ *         URL: http://users.ox.ac.uk/~engs1058/
+ * %              _
+ * %  \/\ /\ /   /  * _  '
+ * % _/\ \/\/ __/__.'(_|_|_
+ */
+
+
+#ifndef TEST_H_
+#define TEST_H_
+
+int testfunc(int x);
+
+#endif /* TEST_H_ */
